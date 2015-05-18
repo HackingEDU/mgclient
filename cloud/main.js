0f5f6293d46fc7bafb0451c18fe2c543d55169a4
STOUT <dook@stardust.red>
2015-05-18 00:16:52 -0700

    #3 Engine switched to Jade

--- cloud/main.js
+++ cloud/main.js
@@ -1,14 +1,13 @@
 // Entry point for Cloud Code
 
-// These two lines are required to initialize Express in Cloud Code.
 var express = require("express");
 var app = express();
 var routes = require("cloud/routes");
 
 // Global app configuration section
 app.set("views", "cloud/views"); // Specify the folder to find templates
-app.set("view engine", "ejs");  // Set the template engine
-app.use(express.bodyParser());   // Middleware for reading request body
+app.set("view engine", "jade");  // Set the template engine
+app.use(express.bodyParser());    // Middleware for reading request body
 
 // Set up routes
 app.get('/index', routes.index);
